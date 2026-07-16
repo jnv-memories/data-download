d9676c7f25d00fefd5bcd040389784f3e96f3fb4
kavaii._aayu <jnv.memories.part01@gmail.com>
2026-07-16 15:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from routes.admin import admin_bp
+from pathlib import Path
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(admin_bp)
 
+
+@admin_bp.get("/debug")
+def debug():
+
+    return {
+        "cwd": os.getcwd(),
+        "files": os.listdir("."),
+        "details_exists": Path("details.xml").exists(),
+        "teacher_exists": Path("teacher_details.xml").exists(),
+        "details_xlsx": Path("details.xlsx").exists(),
+        "teacher_xlsx": Path("teacher_details.xlsx").exists(),
+    }
 @app.route("/")
 def home():
     return {
